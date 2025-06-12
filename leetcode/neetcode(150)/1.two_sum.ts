9caa1f2f3bc2749fbf367a60e0d2626da3beb7f7
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2025-06-12 15:27:51 +0200

    Solve problem 1 of Leetcode for neetcode

---
+++ leetcode/neetcode(150)/1.two_sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
+    for(let i=0 ; i<nums.length ; i++) {
+        const index = map.get(target - nums[i]);
+
+        if(index != undefined) {
+            return [index, i]
+        }
+
+        map.set(nums[i], i);
+    }
+
+    return [-1, -1]
+};
